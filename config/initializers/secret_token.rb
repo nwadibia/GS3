188ae916e4c684dba1f5f3cdb6957a6fb066fd31
Emeka <enwadibia@gmail.com>
2014-05-17 01:36:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GS3::Application.config.secret_key_base = '3b4f42f0b5e925e225026404c57eb052ae3e1d819a7c4f141bb4aae5247fae316f051891e85e97025457021a6fbbf94c2122243d816d163dba12a91c514def36'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GS3::Application.config.session_store :cookie_store, key: '_GS3_session'
